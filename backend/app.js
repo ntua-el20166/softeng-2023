dc4f1c64d734157d7f90a38d1a32bdfb37fe2105
ntua-el20844 <el20844@mail.ntua.gr>
2024-02-01 04:10:09 +0200

    searchtitle

--- backend/app.js
+++ backend/app.js
@@ -50,140 +50,149 @@ app.get("/ntuaflix_api/searchtitle", (req, res) => {
 
       console.log("AAAAAAAAAAAAAAAAAAAA");
       console.log(results);
-      var to_send = results.map((jsonObject) => {
-        if (jsonObject.media_type == "person") {
-          return null;
-        }
-        if (jsonObject.media_type == "tv") {
-          var series_id = jsonObject.id;
-          var genres;
-          var alternative_titles;
-          var rating = jsonObject.vote_average;
-          var votes = jsonObject.vote_count;
-          var start_year;
-          var last_year;
-          var principals;
-          axios
-            .get(`${url}/tv/${series_id}?api_key=${api_key}`)
-            .then((response) => {
-              genres = response.data.genres.map((genre) => {
-                return { genreTitle: genre.name };
+      const to_send = Promise.all(
+        results.map(async (jsonObject) => {
+          if (jsonObject.media_type == "person") {
+            return null;
+          }
+          if (jsonObject.media_type == "tv") {
+            var series_id = jsonObject.id;
+            var genres;
+            var alternative_titles;
+            var rating = jsonObject.vote_average;
+            var votes = jsonObject.vote_count;
+            var start_year;
+            var last_year;
+            var principals;
+            await axios
+              .get(`${url}/tv/${series_id}?api_key=${api_key}`)
+              .then((response) => {
+                genres = response.data.genres.map((genre) => {
+                  return { genreTitle: genre.name };
+                });
+                // rating = response.data.vote_average;
+                // votes = response.data.vote_count;
+                start_year = response.data.first_air_date.substring(0, 4);
+                last_year = response.data.last_air_date.substring(0, 4);
               });
-              // rating = response.data.vote_average;
-              // votes = response.data.vote_count;
-              start_year = response.data.first_air_date.substring(0, 4);
-              last_year = response.data.last_air_date.substring(0, 4);
-            });
 
-          ///////before
-          axios
-            .get(`${url}/tv/${series_id}/alternative_titles?api_key=${api_key}`)
-            .then((response) => {
-              alternative_titles =
-                response.data.titles?.map((title) => {
+            ///////before
+            await axios
+              .get(
+                `${url}/tv/${series_id}/alternative_titles?api_key=${api_key}`
+              )
+              .then((response) => {
+                alternative_titles =
+                  response.data.titles?.map((title) => {
+                    return {
+                      AkaTitle: title.title,
+                      regionAbbr: title.iso_3166_1,
+                    };
+                  }) || [];
+              });
+            await axios
+              .get(`${url}/tv/${series_id}/credits?api_key=${api_key}`)
+              .then((response) => {
+                const cast = response.data.cast;
+                var one = cast.map((person) => {
                   return {
-                    AkaTitle: title.title,
-                    regionAbbr: title.iso_3166_1,
+                    nameID: person.id,
+                    name: person.name,
+                    category: person.known_for_department,
                   };
-                }) || [];
-            });
-          axios
-            .get(`${url}/tv/${series_id}/credits?api_key=${api_key}`)
-            .then((response) => {
-              const cast = response.data.cast;
-              var one = cast.map((person) => {
-                return {
-                  nameID: person.id,
-                  name: person.name,
-                  category: person.known_for_department,
-                };
+                });
+                const crew = response.data.crew;
+                var two = crew.map((person) => {
+                  return {
+                    nameID: person.id,
+                    name: person.name,
+                    category: person.known_for_department,
+                  };
+                });
+                principals = one.concat(two);
               });
-              const crew = response.data.cast;
-              var two = crew.map((person) => {
-                return {
-                  nameID: person.id,
-                  name: person.name,
-                  category: person.known_for_department,
-                };
+            return new titleObject(
+              jsonObject.id,
+              jsonObject.media_type,
+              jsonObject.original_name,
+              jsonObject.poster_path,
+              start_year,
+              last_year,
+              genres,
+              alternative_titles,
+              principals,
+              { avRating: rating, nVotes: votes }
+            );
+          }
+          if (jsonObject.media_type == "movie") {
+            var movie_id = jsonObject.id;
+            var genres;
+            var alternative_titles;
+            var rating = jsonObject.vote_average;
+            var votes = jsonObject.vote_count;
+            var principals;
+            await axios
+              .get(`${url}/movie/${movie_id}?api_key=${api_key}`)
+              .then((response) => {
+                genres = response.data.genres.map((genre) => {
+                  return { genreTitle: genre.name };
+                });
+                // rating = response.data.vote_average;
+                // votes = response.data.vote_count;
               });
-              principals = one.concat(two);
-            });
-          return new titleObject(
-            jsonObject.id,
-            jsonObject.media_type,
-            jsonObject.original_name,
-            jsonObject.poster_path,
-            start_year,
-            last_year,
-            genres,
-            alternative_titles,
-            principals,
-            { avRating: rating, nVotes: votes }
-          );
-        }
-        if (jsonObject.media_type == "movie") {
-          var movie_id = jsonObject.id;
-          var genres;
-          var alternative_titles;
-          var rating = jsonObject.vote_average;
-          var votes = jsonObject.vote_count;
-          var principals;
-          axios
-            .get(`${url}/movie/${movie_id}?api_key=${api_key}`)
-            .then((response) => {
-              genres = response.data.genres.map((genre) => {
-                return { genreTitle: genre.name };
+            await axios
+              .get(
+                `${url}/movie/${movie_id}/alternative_titles?api_key=${api_key}`
+              )
+              .then((response) => {
+                alternative_titles =
+                  response.data.titles?.map((title) => {
+                    return {
+                      AkaTitle: title.title,
+                      regionAbbr: title.iso_3166_1,
+                    };
+                  }) || [];
               });
-              // rating = response.data.vote_average;
-              // votes = response.data.vote_count;
-            });
-          axios
-            .get(
-              `${url}/movie/${movie_id}/alternative_titles?api_key=${api_key}`
-            )
-            .then((response) => {
-              alternative_titles =
-                response.data.titles?.map((title) => {
-                  return { AkaTitle: title, regionAbbr: title.iso_3166_1 };
-                }) || [];
-            });
-          axios
-            .get(`${url}/movie/${movie_id}/credits?api_key=${api_key}`)
-            .then((response) => {
-              const cast = response.data.cast;
-              var one = cast.map((person) => {
-                return {
-                  nameID: person.id,
-                  name: person.name,
-                  category: person.known_for_department,
-                };
+            await axios
+              .get(`${url}/movie/${movie_id}/credits?api_key=${api_key}`)
+              .then((response) => {
+                const cast = response.data.cast;
+                var one = cast.map((person) => {
+                  return {
+                    nameID: person.id,
+                    name: person.name,
+                    category: person.known_for_department,
+                  };
+                });
+                const crew = response.data.crew;
+                var two = crew.map((person) => {
+                  return {
+                    nameID: person.id,
+                    name: person.name,
+                    category: person.known_for_department,
+                  };
+                });
+                principals = one.concat(two);
               });
-              const crew = response.data.cast;
-              var two = crew.map((person) => {
-                return {
-                  nameID: person.id,
-                  name: person.name,
-                  category: person.known_for_department,
-                };
-              });
-              principals = one.concat(two);
-            });
-          return new titleObject(
-            jsonObject.id,
-            jsonObject.media_type,
-            jsonObject.original_title,
-            jsonObject.poster_path,
-            jsonObject.release_date.substring(0, 4),
-            null,
-            genres,
-            alternative_titles,
-            principals,
-            { avRating: rating, nVotes: votes }
-          );
-        }
+            return new titleObject(
+              jsonObject.id,
+              jsonObject.media_type,
+              jsonObject.original_title,
+              jsonObject.poster_path,
+              jsonObject.release_date.substring(0, 4),
+              null,
+              genres,
+              alternative_titles,
+              principals,
+              { avRating: rating, nVotes: votes }
+            );
+          }
+        })
+      );
+      to_send.then((data) => {
+        res.send(data.filter(Boolean));
       });
-
-      res.send(to_send);
+      //res.send(to_send);
     });
 
   //imdb api call
