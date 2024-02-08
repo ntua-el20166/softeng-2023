7fbc95cecbff7151f4fc8235db16fe788dc1e98b
ntua-el20176 <el20176@mail.ntua.gr>
2024-02-08 03:55:43 +0200

    fixed csv format

--- backend/script_files/csvformat.js
+++ backend/script_files/csvformat.js
@@ -1,11 +1,4 @@
-const path = require("path");
-const { errorHandler } = require("./errorHandler.js");
-const fs = require("fs");
-
 async function csvformat(data, res) {
-  const csvDirectory = path.join(__dirname, "../output/");
-  const csvFilePath = path.join(csvDirectory, "output.csv");
-
   var items = JSON.stringify(data);
   var replacer = function (key, value) {
     return value === null ? "" : value;
@@ -22,33 +15,8 @@ async function csvformat(data, res) {
       .join(","),
   ].join("\r\n");
 
-  fs.writeFile(csvFilePath, csvContent, "utf8", (err) => {
-    if (err) {
-      console.error("Error writing CSV file:", err);
-      errorHandler(err, res); // Handle the error appropriately
-    } else {
-      // Send the CSV file as a response
-      res.setHeader("Content-Type", "text/csv");
-      res.attachment("name.csv");
-
-      const cleanupCsvFile = () => {
-        fs.unlink(csvFilePath, (error) => {
-          if (error) {
-            console.error("Error deleting CSV file:", error);
-          } else {
-          }
-        });
-      };
-
-      res.status(200).sendFile(csvFilePath, {}, (err) => {
-        if (err) {
-          errorHandler(err, res);
-        } else {
-          cleanupCsvFile();
-        }
-      });
-    }
-  });
+  res.setHeader("Content-Type", "text/csv");
+  res.status(200).send(csvContent);
 }
 
 module.exports = {
